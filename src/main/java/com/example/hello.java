4f99d38bb0619414467db7775b08eca7f2669cf1
kushal19-alt <kushalsanjay1234@gmail.com>
2025-11-20 14:57:09 +0530

    Create hello.java

---
+++ src/main/java/com/example/hello.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, DevOps CI/CD using GitHub Actions! 2nd trial");
+
+        System.out.println("Change #1: Verifying automatic workflow trigger on push.");
+
+    }
+
+}
